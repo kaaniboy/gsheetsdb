3059c1840ada31ff854d042b44e714de18d56ed8
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-04-29 16:38:45 -0700

    Remove private methods from SheetsTable

--- src/SheetsTable.ts
+++ src/SheetsTable.ts
@@ -5,78 +5,25 @@ import { SheetsTableSchema, SheetsTableColumn } from './Types';
 type ColumnMappings = {[key: string]: string; };
 
 export default class SheetsTable {
-    _columnMappings: ColumnMappings;
-    
     constructor(
         public db: SheetsDB,
         public schema: SheetsTableSchema
-    ) {
-        this._columnMappings = 
-            this._createColumnMappings(schema.cols);
-    }
+    ) { }
 
     async query(query: string) {
-        query = query.toLowerCase() 
-            + ' label ' + this._createLabelsListing(query);
-        query = this._mapQueryColumnNames(query);
+        query = this._prepareQuery(query);
 
         return await new SheetsQuery(this).run(query);
     }
 
-    _createColumnMappings(cols: SheetsTableColumn[] ): ColumnMappings {
-        const mappings: ColumnMappings = {};
-        let i = 0;
-
-        for (let c of cols) {
-            const sheetColName = String.fromCharCode(65 + (i++));
-            mappings[c.name] = sheetColName;
+    _prepareQuery(query: string): string {
+        const labelsListing = createLabelsListing(query);
+        if (labelsListing.length === 0) {
+            return '';
         }
-        
-        return mappings;
-    }
-
-    _mapQueryColumnNames(query: string): string {
-        let offset = 0;
-        for (let { name } of this.schema.cols) {
-            query = query.replace(
-                new RegExp(`\\|${name.toLowerCase()}\\|`, 'g'),
-                String.fromCharCode(65 + (offset++))
-            );
-        }
-        return query;
-    }
-
-    _createLabelsListing(query: string): string {
-        return this._createLabels(query).join(', ')
-    }
-
-    _createLabels(query: string): string[] {
-        const colsListing = this._extractColsListing(query);
-        return colsListing.map(col => {
-            const extractedCol = col.replace(/\|/g, '');
-            return `${col} '${extractedCol}'`;
-        })
-    }
-
-    _extractColsListing(query: string): string[] {
-        const colsListingStart = 'select'.length;
-        let colsListingEnd = query.length;
-
-        for (let term of COLS_LISTING_END) {
-            if (query.includes(term)) {
-                colsListingEnd = Math.min(colsListingEnd, query.indexOf(term));
-            }
-        }
-
-        const colsListing = query.substring(
-            colsListingStart, 
-            colsListingEnd
-        ).trim().replace(/\s/g, '');
-
-        if (colsListing.length === 0) {
-            return [];
-        }
-        return colsListing.split(',');
+        query = query.toLowerCase() 
+            + ' label ' + labelsListing;
+        return mapQueryColumnNames(query, this);
     }
 }
 
@@ -85,3 +32,47 @@ const COLS_LISTING_END = [
     'order by', 'limit', 'offset', 
     'label', 'format', 'options'
 ];
+
+function mapQueryColumnNames(query: string, table: SheetsTable): string {
+    let offset = 0;
+    for (let { name } of table.schema.cols) {
+        query = query.replace(
+            new RegExp(`\\|${name.toLowerCase()}\\|`, 'g'),
+            String.fromCharCode(65 + (offset++))
+        );
+    }
+    return query;
+}
+
+function createLabelsListing(query: string): string {
+    return createLabels(query).join(', ')
+}
+
+function createLabels(query: string): string[] {
+    const colsListing = extractColsListing(query);
+    return colsListing.map(col => {
+        const extractedCol = col.replace(/\|/g, '');
+        return `${col} '${extractedCol}'`;
+    });
+}
+
+function extractColsListing(query: string): string[] {
+    const colsListingStart = 'select'.length;
+    let colsListingEnd = query.length;
+
+    for (let term of COLS_LISTING_END) {
+        if (query.includes(term)) {
+            colsListingEnd = Math.min(colsListingEnd, query.indexOf(term));
+        }
+    }
+
+    const colsListing = query.substring(
+        colsListingStart, 
+        colsListingEnd
+    ).trim().replace(/\s/g, '');
+
+    if (colsListing.length === 0) {
+        return [];
+    }
+    return colsListing.split(',');
+}
--- tests/SheetsTable.test.ts
+++ tests/SheetsTable.test.ts
@@ -4,24 +4,24 @@ describe('_createLabelsListing', () => {
     it('handles zero column SELECT', () => {
         const query = '';
         const expected = '';
-        expect(usersTable._createLabelsListing(query)).toEqual(expected);
+        expect(usersTable._prepareQuery(query)).toEqual(expected);
     });
 
     it('handles single column SELECT', () => {
         const query = 'select |id|';
-        const expected = "|id| 'id'";
-        expect(usersTable._createLabelsListing(query)).toEqual(expected);
+        const expected = "select A label A 'id'";
+        expect(usersTable._prepareQuery(query)).toEqual(expected);
     });
 
     it('handles multi column SELECT', () => {
         const query = 'select |id|, |name|';
-        const expected = "|id| 'id', |name| 'name'";
-        expect(usersTable._createLabelsListing(query)).toEqual(expected);
+        const expected = "select A, B label A 'id', B 'name'";
+        expect(usersTable._prepareQuery(query)).toEqual(expected);
     });
 
     it('handles functions in SELECT', () => {
         const query = 'select max(|id|)';
-        const expected = "max(|id|) 'max(id)'";
-        expect(usersTable._createLabelsListing(query)).toEqual(expected);
+        const expected = "select max(A) label max(A) 'max(id)'";
+        expect(usersTable._prepareQuery(query)).toEqual(expected);
     });
 });
